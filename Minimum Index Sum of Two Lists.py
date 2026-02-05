5c5cc725650bb14cacfd8695eeafbb94c8010ae6
Robel Wondwosen <121330659+Robel-w@users.noreply.github.com>
2026-02-05 17:11:26 +0300

    Update Minimum Index Sum of Two Lists.py

--- Minimum Index Sum of Two Lists.py
+++ Minimum Index Sum of Two Lists.py
@@ -1,27 +1,27 @@
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
-        # mapp = {}
-        # res = []
-        # for i in range(len(list1)):
-        #     for j in range(len(list2)):
-        #         if list1[i] == list2[j]:
-        #             mapp[list1[i]] = i + j
-        # minn = float('inf')            
-        # for a, b in mapp.items():
-        #     minn = min(minn, b)
-        # for a, b in mapp.items():
-        #     if b == minn:
-        #         res.append(a)
-        # return res        
-        minn = float('inf')
+        mapp = {}
         res = []
         for i in range(len(list1)):
-            if list1[i] in list2:
-                j = list2.index(list1[i])
-                if i + j < minn:
-                    minn = i+j
-                    res=[list1[i]]
-                elif i + j == minn:
-                    res.append(list1[i]) 
+            for j in range(len(list2)):
+                if list1[i] == list2[j]:
+                    mapp[list1[i]] = i + j
+        minn = float('inf')            
+        for a, b in mapp.items():
+            minn = min(minn, b)
+        for a, b in mapp.items():
+            if b == minn:
+                res.append(a)
+        return res        
+        # minn = float('inf')
+        # res = []
+        # for i in range(len(list1)):
+        #     if list1[i] in list2:
+        #         j = list2.index(list1[i])
+        #         if i + j < minn:
+        #             minn = i+j
+        #             res=[list1[i]]
+        #         elif i + j == minn:
+        #             res.append(list1[i]) 
     
-        return res 
+        # return res 
