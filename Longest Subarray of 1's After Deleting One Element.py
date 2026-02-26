1b8cc4334af44d622cd9b445616d64251e42b68b
Robel Wondwosen <121330659+Robel-w@users.noreply.github.com>
2026-02-26 19:33:29 +0300

    Create Longest Subarray of 1's After Deleting One Element.py

---
+++ Longest Subarray of 1's After Deleting One Element.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        summ = 0
+        j = 0
+        maxx = 0
+        for i in range(len(nums)):
+            summ+=nums[i]
+
+            while summ < i-j:
+                summ-=nums[j]
+                j+=1
+                
+            maxx = max(maxx,summ)
+
+        if maxx == len(nums):
+            maxx-=1
+        return maxx
